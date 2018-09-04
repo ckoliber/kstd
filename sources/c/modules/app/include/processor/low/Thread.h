52b4fc5b9df9f78479ded3b34c9ddf07db8346e6
KoLiBer <koliberr136a1@gmail.com>
2018-09-04 10:14:20 -0700

    Windows Thread Refactor

--- sources/c/modules/app/include/processor/low/Thread.h
+++ sources/c/modules/app/include/processor/low/Thread.h
@@ -1,11 +1,11 @@
 #include <memory/low/Type.h>
 
 typedef struct Thread {
-    SignedInt (*start)(struct Thread* self, void* (*function)(void*), void* arg);
-    Void* (*join)(struct Thread* self);
+    SignedInt (*start)(struct Thread* self, Void* (*function)(Void*), Void* arg);
+    SignedInt (*join)(struct Thread* self);
     SignedInt (*id)(struct Thread* self);
     SignedInt (*stop)(struct Thread* self);
 } Thread;
 
-struct Thread* thread_new();
-void thread_free(struct Thread* thread);
+Thread* thread_new();
+void thread_free(Thread* thread);
